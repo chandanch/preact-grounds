2cb5feacaa43e26b8a1c854ba9eaf39797bceda0
chandanch <chandan.cec107@gmail.com>
2024-10-30 13:37:12 +0530

    use of event handlers :bulb:

--- src/index.jsx
+++ src/index.jsx
@@ -2,14 +2,19 @@ import { render } from 'preact';
 import { html } from 'htm/preact';
 
 export function App() {
-	const appVersion = Math.random();
-	const appDev = html`<p>chandanch ${Math.random()}</p>`;
+	const appVersion = '1.0.0';
+	const appDev = html`<p>@chandanch${Math.floor(Math.random() * 100)}</p>`;
+
+	const getRandomValue = () => {
+		alert(`Current Value is ${Math.random()}`);
+	};
 
 	return (
 		<div>
 			<p class="big">Hello World!</p>
 			<h2>App Version: {appVersion} </h2>
 			{appDev}
+			<button onClick={getRandomValue}>Check Count</button>
 		</div>
 	);
 }

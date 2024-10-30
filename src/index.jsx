9df93d70742e94b31e309662d0f1f3f36a8c5a96
chandanch <chandan.cec107@gmail.com>
2024-10-30 13:14:42 +0530

    use of htm to create elements

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,17 @@
-import { render } from "preact";
+import { render } from 'preact';
+import { html } from 'htm/preact';
 
 export function App() {
-  return <p class="big">Hello World!</p>;
+	const appVersion = Math.random();
+	const appDev = html`<p>chandanch ${Math.random()}</p>`;
+
+	return (
+		<div>
+			<p class="big">Hello World!</p>
+			<h2>App Version: {appVersion} </h2>
+			{appDev}
+		</div>
+	);
 }
 
-render(<App />, document.getElementById("app"));
+render(<App />, document.getElementById('app'));

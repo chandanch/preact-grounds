60972550c967c0a7195690674a3d0f16d9953dd2
chandanch <chandan.cec107@gmail.com>
2024-10-31 20:47:10 +0530

    use of class components

--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,13 @@
 import { render } from 'preact';
 
 import AppInfo from './components/appInfo';
+import CustomButton from './components/CustomButton';
 
 export function App() {
 	return (
 		<div>
 			<AppInfo />
+			<CustomButton>Class Button</CustomButton>
 		</div>
 	);
 }

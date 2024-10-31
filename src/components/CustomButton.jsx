3de9b8ff86c734ea5991801aa365d61c416032c5
chandanch <chandan.cec107@gmail.com>
2024-10-31 20:50:46 +0530

    define class component

--- src/components/CustomButton.jsx
+++ src/components/CustomButton.jsx
@@ -1,6 +1,10 @@
 import { Component } from 'preact';
 
 class CustomButton extends Component {
+	componentDidMount() {
+		console.log('component mounted');
+	}
+
 	render(props) {
 		return <button class="my-button">{props.children}</button>;
 	}

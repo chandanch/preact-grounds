f159e8219779a44226d57dbf0e1b8db1e0fcd4e3
chandanch <chandan.cec107@gmail.com>
2024-11-03 08:42:31 +0530

    use of counter button

--- src/index.jsx
+++ src/index.jsx
@@ -2,12 +2,14 @@ import { render } from 'preact';
 
 import AppInfo from './components/appInfo';
 import CustomButton from './components/CustomButton';
+import CounterButton from './components/counterButton';
 
 export function App() {
 	return (
 		<div>
 			<AppInfo />
 			<CustomButton>Class Button</CustomButton>
+			<CounterButton />
 		</div>
 	);
 }

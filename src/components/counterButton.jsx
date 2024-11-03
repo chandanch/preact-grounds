1b441a62b8ccddf4bef75d12e46463c7969bfdb7
chandanch <chandan.cec107@gmail.com>
2024-11-03 08:42:24 +0530

    use of state to store comp values

--- src/components/counterButton.jsx
+++ src/components/counterButton.jsx
@@ -1,5 +1,15 @@
+import { useState } from 'preact/hooks';
+
 const CounterButton = () => {
-	return <div>Conter</div>;
+	const [counter, setCounter] = useState(0);
+
+	const incrementCounter = () => {
+		setCounter(counter + 1);
+	};
+
+	return (
+		<button onClick={incrementCounter}>Add one! Current: {counter} </button>
+	);
 };
 
 export default CounterButton;

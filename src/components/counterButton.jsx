94fd2e8531883259a522255d3a7ad7ed597ff8ba
chandanch <chandan.cec107@gmail.com>
2024-11-04 18:38:52 +0530

    add isactive state

--- src/components/counterButton.jsx
+++ src/components/counterButton.jsx
@@ -2,13 +2,20 @@ import { useState } from 'preact/hooks';
 
 const CounterButton = () => {
 	const [counter, setCounter] = useState(0);
+	const [isActive, setActive] = useState(true);
 
 	const incrementCounter = () => {
 		setCounter(counter + 1);
+		setActive(!isActive);
+		setTimeout(() => {
+			setActive(!isActive);
+		}, 3000);
 	};
 
 	return (
-		<button onClick={incrementCounter}>Add one! Current: {counter} </button>
+		<button onClick={incrementCounter} disabled={isActive}>
+			Add one! Current: {counter}{' '}
+		</button>
 	);
 };
 

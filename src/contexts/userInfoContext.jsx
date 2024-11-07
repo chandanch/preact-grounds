256a0a9bbc0656d619528a23d0cb5474a4c68d87
chandanch <chandan.cec107@gmail.com>
2024-11-07 07:03:24 +0530

    custom hook to return context

--- src/contexts/userInfoContext.jsx
+++ src/contexts/userInfoContext.jsx
@@ -1,6 +1,9 @@
 import { createContext } from 'preact';
+import { useContext } from 'preact/hooks';
 
 const UserInfoContext = createContext(null);
+
+export const useUserInfoContext = () => useContext(UserInfoContext);
 
 export const UserInfoContextProvider = ({ children }) => {
 	return (
